5570ddd2563921dcece1741bef136e79cfd1df1b
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2023-11-06 18:39:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
-  
-  
+  const tr1 = document.createElement("tr");
+	const td1 = document.createElement("td");
+	td1.innerText = "New Cell1";
+	const td2 = document.createElement("td");
+	td2.innerText = "New Cell2";
+
+	tr1.appendChild(td1);
+	tr1appendChild(td2);
+	table.insertBefore(tr1,tr);
 }
